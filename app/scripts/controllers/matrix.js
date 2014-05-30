b2b72b68235c0ba97c0ab7d707447c8178265568
Adam Leeper <aleeper@hidof.com>
2014-05-29 21:30:03 -0700

    Rotation matrix can now set master params.
    Also added some section headers.

--- app/scripts/controllers/matrix.js
+++ app/scripts/controllers/matrix.js
@@ -2,28 +2,41 @@
 
 angular.module('rotationAppApp')
   .controller('MatrixCtrl', function ($scope) {
-    $scope.matrix = {}
+    $scope.matrix = {};
 
-    $scope.$watch('masterParams', function(value) {
-      var q, m;
-
+    var updateMatrix = function(value) {
       console.log('watched');
-
-      q = new THREE.Quaternion(value.x, value.y, value.z, value.w);
-      m = new THREE.Matrix4();
+      var q = new THREE.Quaternion(value.x, value.y, value.z, value.w);
+      var m = new THREE.Matrix4();
 
       m.makeRotationFromQuaternion(q);
       $scope.matrix = {
-        xx: m.elements[0],
-        xy: m.elements[4],
-        xz: m.elements[8],
-        yx: m.elements[1],
-        yy: m.elements[5],
-        yz: m.elements[9],
-        zx: m.elements[2],
-        zy: m.elements[6],
-        zz: m.elements[10]
+          xx: m.elements[0],
+          xy: m.elements[4],
+          xz: m.elements[8],
+          yx: m.elements[1],
+          yy: m.elements[5],
+          yz: m.elements[9],
+          zx: m.elements[2],
+          zy: m.elements[6],
+          zz: m.elements[10]
       };
-    }, true);
+    };
+
+    $scope.setFromMatrix = function() {
+      console.log('setFromMatrix');
+      var matrix = $scope.matrix;
+      var m = new THREE.Matrix4(matrix.xx, matrix.xy, matrix.xz, 0,
+                                matrix.yx, matrix.yy, matrix.yz, 0,
+                                matrix.zx, matrix.zy, matrix.zz, 0,
+                                        0,         0,         0, 1);
+
+      var q = new THREE.Quaternion();
+      q.setFromRotationMatrix(m);
+      $scope.$parent.masterParams = {w: q.w, x: q.x, y: q.y, z: q.z};
+    };
+
+    // Set up a watcher so that the matrix will update when the master params change.
+    $scope.$watch('masterParams', updateMatrix , true);
 
   });
--- app/scripts/controllers/quaternion.js
+++ app/scripts/controllers/quaternion.js
@@ -2,5 +2,13 @@
 
 angular.module('rotationAppApp')
   .controller('QuaternionCtrl', function ($scope) {
-    $scope.quaternion = $scope.$parent.masterParams
+    $scope.quaternion = $scope.$parent.masterParams;
+
+    var updateQuaternion = function(value) {
+      $scope.quaternion = value;
+    };
+
+    // Set up a watcher so that the data will update when the master params change.
+    $scope.$watch('masterParams', updateQuaternion , true);
+
   });
