1e28f1ed53721b300d3e160bd7bd4c314b6fef9e
Adam Leeper <aleeper@hidof.com>
2014-05-29 23:33:33 -0700

    add angle-axis, and disable watchers during update

--- app/scripts/controllers/matrix.js
+++ app/scripts/controllers/matrix.js
@@ -2,41 +2,51 @@
 
 angular.module('rotationAppApp')
   .controller('MatrixCtrl', function ($scope) {
-    $scope.matrix = {};
 
-    var updateMatrix = function(value) {
-      console.log('watched');
-      var q = new THREE.Quaternion(value.x, value.y, value.z, value.w);
+    var updateMatrix = function(newValue, oldValue) {
+      if (newValue === oldValue)
+        return;
+
+      console.log('updateMatrix');
+      var q = new THREE.Quaternion(newValue.x, newValue.y, newValue.z, newValue.w);
       var m = new THREE.Matrix4();
-
       m.makeRotationFromQuaternion(q);
+      // Note three.js matrices store elements in column-major order.
       $scope.matrix = {
-          xx: m.elements[0],
-          xy: m.elements[4],
-          xz: m.elements[8],
-          yx: m.elements[1],
-          yy: m.elements[5],
-          yz: m.elements[9],
-          zx: m.elements[2],
-          zy: m.elements[6],
-          zz: m.elements[10]
-      };
+          xx: m.elements[0], xy: m.elements[4], xz: m.elements[8],
+          yx: m.elements[1], yy: m.elements[5], yz: m.elements[9],
+          zx: m.elements[2], zy: m.elements[6], zz: m.elements[10]};
     };
 
-    $scope.setFromMatrix = function() {
-      console.log('setFromMatrix');
+    $scope.setMasterParams = function() {
+      console.log('setMasterParams from matrix');
+
+      // Do the math
       var matrix = $scope.matrix;
+      // Oddly enough, the three.js matrix constructor is row-major.
       var m = new THREE.Matrix4(matrix.xx, matrix.xy, matrix.xz, 0,
                                 matrix.yx, matrix.yy, matrix.yz, 0,
                                 matrix.zx, matrix.zy, matrix.zz, 0,
                                         0,         0,         0, 1);
-
       var q = new THREE.Quaternion();
       q.setFromRotationMatrix(m);
+
+      // Do the update.
+      $scope.stopWatching();
       $scope.$parent.masterParams = {w: q.w, x: q.x, y: q.y, z: q.z};
+      $scope.startWatching();
     };
 
-    // Set up a watcher so that the matrix will update when the master params change.
-    $scope.$watch('masterParams', updateMatrix , true);
+    $scope.startWatching = function() {
+      $scope.stopWatching = $scope.$watch('masterParams', updateMatrix , true);
+    };
+
+    // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = //
+
+    // Initialize this representation.
+    updateMatrix($scope.$parent.masterParams);
+
+    // Start watching the masterParams
+    $scope.startWatching();
 
   });
